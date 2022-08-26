b2efaeb4cef6cc1727e4220f2d51b70c21a3c386
Pavel Egorov <xoposhiy@mail.ru>
2022-08-27 02:38:26 +0500

    + worker

---
+++ worker/WorkerEntryPoint.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using lib;
+using lib.db;
+
+namespace worker
+{
+    public static class WorkerEntryPoint
+    {
+        public static void Main()
+        {
+            var releaseTag = EnvironmentVariables.Get("PEGOVKA_RELEASE_TAG");
+            var shardingToken = EnvironmentVariables.Get("PEGOVKA_SHARDING_TOKEN");
+            var timeoutMinutes = EnvironmentVariables.TryGet("PEGOVKA_TIMEOUT_MINUTES", int.Parse, 15);
+            Console.Out.WriteLine($"Worker '{releaseTag}' is processing shard: {shardingToken}");
+
+            // определить задачу по shardingToken
+            // решить задачу
+            // сохранить решение задачи в БД
+        }
+    }
+}
