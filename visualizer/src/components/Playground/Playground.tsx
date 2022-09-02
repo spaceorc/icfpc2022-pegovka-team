7d33de77c4b41f6e976f54081bc3d3af4b69a0d8
Vasilii Kuznetsov <kvas@yandex-team.ru>
2022-09-02 20:02:00 +0500

    get pixel by click and costs

--- visualizer/src/components/Playground/Playground.tsx
+++ visualizer/src/components/Playground/Playground.tsx
@@ -6,14 +6,21 @@ import { instructionToString } from "../../contest-logic/Instruction";
 import { Painter } from "../../contest-logic/Painter";
 import { RandomInstructionGenerator } from "../../contest-logic/RandomInstructionGenerator";
 
+function getMousePos(canvas: any, event: any) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top,
+  };
+}
+
 export const Playground = (): JSX.Element => {
   const [width, setWidth] = useState(400);
   const [height, setHeight] = useState(400);
+  const [cost, setCost] = useState(0);
+  const [result, setResut] = useState();
 
   const [playgroundCode, setPlaygroundCode] = useState("");
-  const [paintedCanvas, setPaintedCanvas] = useState(
-    new Canvas(width, height, new RGBA([255, 255, 255, 255]))
-  );
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const handlePlaygroundCode = (e: any) => {
     setPlaygroundCode(e.target.value as string);
@@ -50,6 +57,7 @@ export const Playground = (): JSX.Element => {
     const context = canvas.getContext("2d")!;
 
     console.log(result.canvas.blocks);
+    setCost(result.cost);
 
     canvas.width = result.canvas.width;
     canvas.height = result.canvas.height;
@@ -66,61 +74,79 @@ export const Playground = (): JSX.Element => {
     setPlaygroundCode("");
     clearCanvas();
   };
+  const onCanvasClick = (event: any) => {
+    console.log(getMousePos(canvasRef.current, event));
+  };
 
   return (
-    <div style={{
-        display: 'flex',
-        maxWidth: '100vw',
-        gap: '20px'
-    }}>
+    <div
+      style={{
+        display: "flex",
+        maxWidth: "100vw",
+        gap: "20px",
+        marginTop: 10
+      }}
+    >
       <div>
         <div>
-            <button onClick={handleClickGenerateInstruction}>Generate Instruction</button>
-            <button onClick={handleClickRenderCanvas}>Render Canvas</button>
-            <button onClick={handleReset}>Reset</button>
+          <button onClick={handleClickGenerateInstruction}>Generate Instruction</button>
+          <button onClick={handleClickRenderCanvas}>Render Canvas</button>
+          <button onClick={handleReset}>Reset</button>
         </div>
         <div>
-            <div>
+          <div>
             <label>
-                width
-                <br />
-                <input
+              width
+              <br />
+              <input
                 type="text"
                 value={width}
                 onChange={(event) => setWidth(Number(event.target.value))}
-                />
+              />
             </label>
             <br />
             <label>
-                height
-                <br />
-                <input
+              height
+              <br />
+              <input
                 type="text"
                 value={height}
                 onChange={(event) => setHeight(Number(event.target.value))}
-                />
+              />
             </label>
             <br />
             <label>
-                code
-                <br />
-                <textarea
+              code
+              <br />
+              <textarea
                 style={{
-                    width: '500px',
-                    height: '400px'
+                  width: "500px",
+                  height: "400px",
                 }}
                 placeholder="Code to be submitted"
                 value={playgroundCode}
                 onChange={handlePlaygroundCode}
-                />
+              />
             </label>
-            </div>
+          </div>
         </div>
       </div>
-      <div style={{
-        flexShrink: 0
-      }}>
-        <canvas style={{ border: "1px solid black" }} width={width} height={height} ref={canvasRef} />
+      <div
+        style={{
+          flexShrink: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 10
+        }}
+      >
+        <canvas
+          style={{ outline: "1px solid black" }}
+          width={width}
+          height={height}
+          ref={canvasRef}
+          onClick={onCanvasClick}
+        />
+        Cost: {cost}
       </div>
     </div>
   );
