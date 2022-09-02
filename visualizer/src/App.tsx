c1dd41dc27e2b761e145a6f4cc115cc4c2b1fac8
Vasilii Kuznetsov <kvas@yandex-team.ru>
2022-09-02 17:40:47 +0500

    template for app

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,53 +1,23 @@
+import { useState } from 'react';
+
 function App() {
-  return (
-    <>
-      <em>
-        Примеры с каждой технологией в <code>main.tsx</code>
-      </em>
-      <ul>
-        <li>
-          Обучалка d3:{" "}
-          <a href="https://observablehq.com/@d3/learn-d3">https://observablehq.com/@d3/learn-d3</a>
-        </li>
-        <li>
-          Примеры использования d3:{" "}
-          <a href="https://observablehq.com/@d3/gallery">https://observablehq.com/@d3/gallery</a>
-        </li>
-        <li>
-          Ещё по d3:{" "}
-          <a href="https://github.com/d3/d3/wiki#resourcesx">
-            https://github.com/d3/d3/wiki#resources
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          Обучалка по Three.js:{" "}
-          <a href="https://threejs.org/manual/#en/fundamentals">
-            https://threejs.org/manual/#en/fundamentals
-          </a>
-        </li>
-        <li>
-          Примеры использования threejs:{" "}
-          <a href="https://threejs.org/examples">https://threejs.org/examples</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          Полезные ссылки по Pixi.js:{" "}
-          <a href="https://github.com/pixijs/pixijs#learn">
-            https://github.com/pixijs/pixijs#learn
-          </a>
-        </li>
-        <li>
-          Viewport для Pixi.js:{" "}
-          <a href="https://github.com/davidfig/pixi-viewport#simple-example">
-            https://github.com/davidfig/pixi-viewport#simple-example
-          </a>
-        </li>
-      </ul>
-    </>
-  );
+    const [width, setWidth] = useState(400);
+    const [height, setHeight] = useState(400);
+    const [code, setCode] = useState('');
+
+    return (
+        <>
+            <div>
+                <label>width<br/><input type="text" value={width} onChange={event => setWidth(Number(event.target.value))} /></label><br/>
+                <label>height<br/><input type="text" value={width} onChange={event => setHeight(Number(event.target.value))} /></label><br/>
+                <label>
+                    code<br/>
+                    <textarea value={code} onChange={event => setCode(event.target.value)} />
+                </label>
+            </div>
+            <canvas width={width} height={height} />
+        </>
+    );
 }
 
 export default App;
