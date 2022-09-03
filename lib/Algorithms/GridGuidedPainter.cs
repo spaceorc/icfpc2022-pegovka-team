b0a4f5ec47ee113408b93f80618fbaf1c1506c13
Pavel Egorov <xoposhiy@mail.ru>
2022-09-03 22:32:32 +0500

    GridGuidedPainter

--- lib/Algorithms/GridGuidedPainter.cs
+++ lib/Algorithms/GridGuidedPainter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace lib.Algorithms;
 
@@ -20,11 +21,92 @@ public class GridGuidedPainter
 
     public (IList<Move>, double Score) GetBestResult()
     {
-        var bl = V.Zero;
+        var start = V.Zero;
+        var rowBottomLeft = start;
         foreach (var gridRow in grid.Rows)
         {
+            var cellBottomLeft = rowBottomLeft;
+            PaintRow(gridRow, cellBottomLeft);
+            rowBottomLeft += V.Down * gridRow.Height;
         }
-        throw new NotImplementedException();
+        return (moves, canvas.GetScore(screen));
     }
 
+    private void PaintRow(GridRow gridRow, V cellBottomLeft)
+    {
+        foreach (var cell in gridRow.Cells)
+        {
+            PaintGridCell(cell, cellBottomLeft, gridRow.Height);
+            cellBottomLeft += V.Right * cell.Width;
+        }
+    }
+
+    private void PaintGridCell(GridCell cell, V bottomLeft, int height)
+    {
+        if (bottomLeft.Y == 0)
+        {
+            if (bottomLeft.X == 0)
+                PaintInitialCell(cell, height);
+            else
+                PaintBottomCell(cell, bottomLeft, height);
+        }
+        else
+        {
+            if (bottomLeft.X == 0)
+                PaintFirstCellInRow(cell, bottomLeft, height);
+            else
+                PaintInnerCell(cell, bottomLeft, height);
+        }
+    }
+
+    private void PaintInnerCell(GridCell cell, V bottomLeft, int height)
+    {
+        var pCut = new PCutMove(Block.Id, bottomLeft);
+        var (bottomLeftBlock, bottomRightBlock, topRightBlock, topLeftBlock) = canvas.ApplyPCut(pCut);
+        moves.Add(pCut);
+        DoColor(cell.Width, height, topRightBlock);
+        var rightBlock = DoMerge(topRightBlock, bottomRightBlock);
+        var leftBlock = DoMerge(topLeftBlock, bottomLeftBlock);
+        DoMerge(rightBlock, leftBlock);
+    }
+
+    private void PaintFirstCellInRow(GridCell cell, V bottomLeft, int height)
+    {
+        var hCut = new HCutMove(Block.Id, bottomLeft.Y);
+        var (bottom, top) = canvas.ApplyHCut(hCut);
+        moves.Add(hCut);
+        DoColor(cell.Width, height, top);
+        DoMerge(bottom, top);
+    }
+
+    private void PaintBottomCell(GridCell cell, V bottomLeft, int height)
+    {
+        var vCut = new VCutMove(Block.Id, bottomLeft.X);
+        var (left, right) = canvas.ApplyVCut(vCut);
+        moves.Add(vCut);
+        DoColor(cell.Width, height, right);
+        DoMerge(left, right);
+    }
+
+    private ComplexBlock DoMerge(Block left, Block right)
+    {
+        var merge = new MergeMove(left.Id, right.Id);
+        moves.Add(merge);
+        return canvas.ApplyMerge(merge);
+    }
+
+    private void PaintInitialCell(GridCell cell, int height)
+    {
+        DoColor(cell.Width, height, Block);
+    }
+
+    private void DoColor(int width, int height, Block block)
+    {
+        var color = screen.GetAverageColorByGeometricMedian(block.Left, block.Bottom, width, height);
+        var colorMove = new ColorMove(block.Id, color);
+        moves.Add(colorMove);
+        canvas.ApplyColor(colorMove);
+    }
+
+    public Block Block => canvas.Blocks.Values.Single();
 }
---
+++ tests/GridGuidedPainterTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using lib;
+using lib.Algorithms;
+using lib.db;
+using NUnit.Framework;
+using TextCopy;
+
+namespace tests;
+
+public class GridGuidedPainterTests
+{
+    [Test]
+    [Parallelizable(ParallelScope.All)]
+    public async Task Run([Range(1, 23)] int problemId)
+    {
+        var bestScore = double.PositiveInfinity;
+
+        foreach (var size in new[]{5, 10, 20, 40, 80})
+        {
+            var screen = Screen.LoadProblem(problemId);
+            var grid = CreateRegularGrid(size);
+            var solver = new GridGuidedPainter(grid, screen);
+            var (moves, score) = solver.GetBestResult();
+            var solution = moves.StrJoin("\n");
+            if (score < bestScore)
+            {
+                Console.WriteLine($"Score: {score} cellSize: {size} movesCount: {moves.Count}");
+                bestScore = score;
+                await ClipboardService.SetTextAsync(solution);
+                await SolutionRepo.SubmitAsync(new ContestSolution(problemId, (int)score, solution, new SolverMeta(),"RegularGrid"));
+            }
+        }
+    }
+
+    private Grid CreateRegularGrid(int count)
+    {
+        var rows = new List<GridRow>();
+        for (int y = 0; y < count; y++)
+        {
+            var cells = new List<GridCell>();
+            for (int x = 0; x < count; x++)
+            {
+                cells.Add(new GridCell(400/count));
+            }
+            rows.Add(new GridRow(400/count, cells));
+        }
+        return new Grid(rows);
+
+    }
+}
