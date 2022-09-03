824be7e959b743dd1951452b3c2092142f0142d8
Alexey Kirpichnikov <alex.kirp@gmail.com>
2022-09-03 20:34:05 +0500

    switch to second enhancer

--- enhancer/Program.cs
+++ enhancer/Program.cs
@@ -19,25 +19,24 @@ namespace enhancer
                 Console.WriteLine("got solutions from DB");
 
                 Parallel.ForEach(scoreByProblemId, parms =>
-                    //foreach (var (problemId, solverId, score) in scoreByProblemId)
                 {
                     var (problemId, solverId, score) = parms;
                     var solution = solutions[(problemId, solverId)];
-                    if (!solution.SolverId.EndsWith("-enchanced") && solution.SolverMeta.Enhancer_Id == null)
+                    if (!solution.SolverId.EndsWith("-enchanced") && solution.SolverMeta.Enhancer_Id != "enchancer2")
                     {
-                        Console.WriteLine($"solution {solution.SolverId} for problem {solution.ProblemId} not enhanced");
+                        Console.WriteLine($"solution {solution.SolverId} for problem {solution.ProblemId} not enhanced2");
                         var screen = ScreenRepo.GetProblem((int)solution.ProblemId);
-                        var eMoves = Enhancer.Enhance(screen, Moves.Parse(solution.Solution));
+                        var eMoves = Enhancer.Enhance2(screen, Moves.Parse(solution.Solution));
                         var eSolution = new ContestSolution(
                             solution.ProblemId,
                             screen.CalculateScore(eMoves),
                             eMoves.StrJoin("\n"),
                             new SolverMeta(solution.ScoreEstimated, solution.SolverId),
-                            solution.SolverId + "-enchanced");
-                        Console.WriteLine($"solution {solution.SolverId} for problem {solution.ProblemId} enhanced from score {solution.ScoreEstimated} to {eSolution.ScoreEstimated}");
+                            solution.SolverId + "-2-enchanced");
+                        Console.WriteLine($"solution {solution.SolverId} for problem {solution.ProblemId} enhanced2 from score {solution.ScoreEstimated} to {eSolution.ScoreEstimated}");
                         SolutionRepo.Submit(eSolution);
 
-                        solution.SolverMeta.Enhancer_Id = "default";
+                        solution.SolverMeta.Enhancer_Id = "enchancer2";
                         SolutionRepo.Submit(solution);
                     }
                 });
