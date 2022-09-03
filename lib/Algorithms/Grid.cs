4dd8e07b1dac09a300a4d80b876d1047668b4908
Pavel Egorov <xoposhiy@mail.ru>
2022-09-03 21:04:47 +0500

    Grid interface

---
+++ lib/Algorithms/Grid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace lib.Algorithms;
+
+public class Grid
+{
+    public Grid(List<GridRow> rows)
+    {
+        Rows = rows;
+    }
+
+    public List<GridRow> Rows;
+
+}
+
+public class GridRow
+{
+    public GridRow(int height, List<GridCell> widths)
+    {
+        Height = height;
+        Widths = widths;
+    }
+
+    public int Height;
+    public List<GridCell> Widths;
+}
+
+public class GridCell
+{
+    public GridCell(int width, int similarityCost)
+    {
+        Width = width;
+        SimilarityCost = similarityCost;
+    }
+
+    public int Width;
+    public int SimilarityCost;
+}
