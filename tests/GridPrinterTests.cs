6e9cebe082e5788ce549b56d3d7c4c37986434d7
Pavel Egorov <xoposhiy@mail.ru>
2022-09-03 12:30:49 +0500

    GridPrinter Tests

--- tests/GridPrinterTests.cs
+++ tests/GridPrinterTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using lib;
 using lib.Algorithms;
+using lib.db;
 using NUnit.Framework;
 using TextCopy;
 
@@ -9,14 +11,15 @@ namespace tests;
 public class GridPrinterTests
 {
     [Test]
-    public void Run([Range(11, 11)] int problemId)
+    public async Task Run([Range(1, 25)] int problemId)
     {
-        var solver = new GridPrinterAlgorithm(40);
+        var solver = new GridPrinterAlgorithm(35);
         var screen = Screen.LoadProblem(problemId);
         var (moves, score) = solver.GetBestResult(screen);
         Console.WriteLine($"Score: {score}");
         var solution = moves.StrJoin("\n");
         ClipboardService.SetText(solution);
         Console.WriteLine(solution);
+        await SolutionRepo.Submit(new ContestSolution(problemId, (int)score, solution, new SolverMeta(), DateTime.Now, nameof(GridPrinterAlgorithm)));
     }
 }
