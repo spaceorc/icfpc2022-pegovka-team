264efcee89009489c005239e9c60304a8f86e8f0
Pavel Egorov <xoposhiy@mail.ru>
2022-09-03 21:10:21 +0500

    LayerAlgo

--- tests/LayersAlgoTests.cs
+++ tests/LayersAlgoTests.cs
@@ -12,18 +12,25 @@ public class LayersAlgoTests
 {
     [Test]
     [Parallelizable(ParallelScope.All)]
-    public async Task Run([Range(1, 25)] int problemId)
+    public async Task Run([Range(1, 1)] int problemId)
     {
         //await SolutionRepo.Submit(new ContestSolution(problemId, (int)29856, File.ReadAllText(@"c:\work\contests\icfpc-2022\hand-solutions\problem-1-grid.txt "), new SolverMeta(), DateTime.Now, nameof(GridPrinterAlgorithm)));
-        foreach (var cellSize in new[]{20, 40})
+        var bestScore = double.PositiveInfinity;
+        foreach (var cellSize in new[]{40})
         {
             var solver = new LayersAlgorithm(cellSize);
-            var screen = Screen.LoadProblem(problemId);
+            var screen = Screen.LoadProblem(problemId).MedianFilter();
+            screen.ToImage("filtered.png");
             var (moves, score) = solver.GetBestResult(screen);
+
             Console.WriteLine($"Score: {score} cellSize: {cellSize}");
             var solution = moves.StrJoin("\n");
-            await ClipboardService.SetTextAsync(solution);
-            await SolutionRepo.SubmitAsync(new ContestSolution(problemId, (int)score, solution, new SolverMeta(), nameof(LayersAlgorithm)+cellSize));
+            if (score < bestScore)
+            {
+                bestScore = score;
+                await ClipboardService.SetTextAsync(solution);
+                await SolutionRepo.SubmitAsync(new ContestSolution(problemId, (int)score, solution, new SolverMeta(), nameof(LayersAlgorithm)+cellSize));
+            }
         }
     }
 }
