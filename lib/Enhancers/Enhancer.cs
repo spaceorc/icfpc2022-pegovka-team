a6a545f5fdecad0e4e5a877f979db232b14fc1f0
spaceorc <spaceorc@yandex.ru>
2022-09-03 20:32:38 +0500

    Fix enhancer

--- lib/Enhancers/Enhancer.cs
+++ lib/Enhancers/Enhancer.cs
@@ -11,8 +11,13 @@ public static class Enhancer
 
     public static List<Move> Enhance2(Screen problem, List<Move> moves)
     {
-        var first = new CombinedEnhancer(new ColorPicker(), new CutEnhancer());
-        var second = new CombinedEnhancer(new CutEnhancer(), new ColorEnhancer());
-        return new CombinedEnhancer(first, second).Enhance(problem, moves);
+        if (problem.InitialBlocks.Length == 1)
+        {
+            var first = new CombinedEnhancer(new ColorPicker(), new CutEnhancer());
+            var second = new CombinedEnhancer(new CutEnhancer(), new ColorEnhancer());
+            return new CombinedEnhancer(first, second).Enhance(problem, moves);
+        }
+
+        return Enhance(problem, moves);
     }
 }
