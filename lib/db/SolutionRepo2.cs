7372ea1e227ac6197f4e569ce41ca5811f66cadd
Pavel Egorov <xoposhiy@mail.ru>
2022-09-05 16:14:18 +0500

    Change db

--- lib/db/SolutionRepo2.cs
+++ lib/db/SolutionRepo2.cs
@@ -1,99 +1,23 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
 using Ydb.Sdk;
 using Ydb.Sdk.Table;
 using Ydb.Sdk.Value;
 using Ydb.Sdk.Yc;
-// ReSharper disable InconsistentNaming
 
 namespace lib.db;
 
-public class ContestSolution
+public class SolutionRepo2
 {
-    public Guid Id;
-    public long? SubmissionId;
-    public long ProblemId;
-    public string SolverId;
-    public long ScoreEstimated;
-    public long? ScoreServer;
-    public string Solution;
-    public SolverMeta SolverMeta;
-    public DateTime SolvedAt;
-    public DateTime? SubmittedAt;
+    private readonly TableClient client;
 
-    public ContestSolution(long problemId, long scoreEstimated, string solution, SolverMeta solverMeta, string solverId)
-    {
-        Id = Guid.NewGuid();
-        ProblemId = problemId;
-        ScoreEstimated = scoreEstimated;
-        Solution = solution;
-        SolverMeta = solverMeta;
-        SolvedAt = DateTime.UtcNow;
-        SolverId = solverId;
-    }
-
-    public override string ToString()
-    {
-        return $"{nameof(Id)}: {Id}, {nameof(SubmissionId)}: {SubmissionId}, {nameof(ProblemId)}: {ProblemId}, {nameof(SolverId)}: {SolverId}, {nameof(ScoreEstimated)}: {ScoreEstimated}, {nameof(ScoreServer)}: {ScoreServer}, {nameof(Solution)}: {Solution}, {nameof(SolverMeta)}: {SolverMeta}, {nameof(SolvedAt)}: {SolvedAt}, {nameof(SubmittedAt)}: {SubmittedAt}";
-    }
-
-    public ContestSolution(ResultSet.Row row)
-    {
-        var id = (string?) row["id"] ?? throw new ArgumentException();
-        var solverMeta = row["solver_meta"].GetOptionalJson() ?? throw new ArgumentException();
-        Id = Guid.Parse(id);
-        ProblemId = (long?) row["problem_id"] ?? throw new ArgumentException();
-        ScoreEstimated = (long?) row["score_estimated"] ?? throw new ArgumentException();
-        Solution = (string?) row["solution"] ?? throw new ArgumentException();
-        SolverMeta = solverMeta.FromJson<SolverMeta>();
-        SolvedAt = (DateTime?) row["solved_at"] ?? throw new ArgumentException();
-        SolverId = (string?) row["solver_id"] ?? throw new ArgumentException();
-
-        SubmissionId = (long?) row["submission_id"];
-        ScoreServer = (long?) row["score_server"];
-        SubmittedAt = (DateTime?) row["submitted_at"];
-    }
-}
-
-public class SolverMeta
-{
-    public long Previous_Score;
-    public string? Previous_SolverName;
-    public string? Description;
-    public List<string>? Enhanced_By;
-
-    public SolverMeta()
-    {
-
-    }
-
-    public SolverMeta(long previousScore, string previousSolverName)
-    {
-        Previous_Score = previousScore;
-        Previous_SolverName = previousSolverName;
-    }
-
-    public override string ToString()
-    {
-        return $"{nameof(Previous_Score)}: {Previous_Score}, {nameof(Previous_SolverName)}: {Previous_SolverName}, {Description}, {nameof(Enhanced_By)}: {(Enhanced_By == null ? "" : string.Join(" ", Enhanced_By!))}";
-    }
-
-    public string ToJson()
-    {
-        return JsonExtensions.ToJson(this);
-    }
-}
-
-public static class SolutionRepo
-{
-    public static async Task SubmitAsync(ContestSolution solution)
+    public async Task SubmitAsync(ContestSolution solution)
     {
         Console.WriteLine("SubmitAsync");
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -109,7 +33,7 @@ public static class SolutionRepo
                 DECLARE $submission_id AS Int64?;
                 DECLARE $submitted_at AS Datetime?;
                 UPSERT INTO Solutions (id, problem_id, score_estimated, score_server,solution, solved_at, solver_id, solver_meta, submission_id, submitted_at) VALUES ($id, $problem_id, $score_estimated, $score_server, $solution, $solved_at, $solver_id, $solver_meta, $submission_id, $submitted_at)",
-                txControl: TxControl.BeginSerializableRW().Commit(),
+                txControl: TxControl.BeginStaleRO().Commit(),
                 parameters: new Dictionary<string, YdbValue>
                 {
                     { "$id", YdbValue.MakeUtf8(solution.Id.ToString())},
@@ -127,16 +51,15 @@ public static class SolutionRepo
         response.Status.EnsureSuccess();
     }
 
-    public static void Submit(ContestSolution solution)
+    public  void Submit(ContestSolution solution)
     {
         SubmitAsync(solution).GetAwaiter().GetResult();
     }
 
-    public static async Task<List<(long problemId, long score)>> GetBestScoreByProblemId()
+    public  async Task<List<(long problemId, long score)>> GetBestScoreByProblemId()
     {
         Console.WriteLine("GetBestScoreByProblemId");
         var ans = new List<(long, long)>();
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -157,11 +80,10 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<List<(long problemId, string solverId, long score)>> GetBestScoreByProblemIdAndSolverId(List<string> ignoreSolverPrefixes)
+    public  async Task<List<(long problemId, string solverId, long score)>> GetBestScoreByProblemIdAndSolverId(List<string> ignoreSolverPrefixes)
     {
         Console.WriteLine("GetBestScoreByProblemIdAndSolverId");
         var ans = new List<(long, string, long)>();
-        var client = await CreateTableClient();
         var ignoreClause = ignoreSolverPrefixes.StrJoin("|");
         var response = await client.SessionExec(async session =>
 
@@ -184,10 +106,9 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<ContestSolution> GetSolutionByProblemIdAndScore(long problemId, long scoreEstimated)
+    public  async Task<ContestSolution> GetSolutionByProblemIdAndScore(long problemId, long scoreEstimated)
     {
         Console.WriteLine("GetSolutionByProblemIdAndScore");
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -212,10 +133,10 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<ContestSolution> GetSolutionByProblemIdAndSolverIdAndScore(long problemId, string solverId, long scoreEstimated)
+    public  async Task<ContestSolution> GetSolutionByProblemIdAndSolverIdAndScore(long problemId, string solverId, long scoreEstimated)
     {
-        Console.WriteLine("GetSolutionByProblemIdAndSolverIdAndScore");
-        var client = await CreateTableClient();
+        Console.Write("GetSolutionByProblemIdAndSolverIdAndScore ");
+        var sw = Stopwatch.StartNew();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -235,6 +156,7 @@ public static class SolutionRepo
 
             ));
         response.Status.EnsureSuccess();
+        Console.WriteLine(sw.Elapsed);
         var queryResponse = (ExecuteDataQueryResponse) response;
         var row = queryResponse.Result.ResultSets[0].Rows.First();
 
@@ -242,10 +164,9 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<ContestSolution?> GetBestSolutionBySolverId(long problemId, string solverId)
+    public  async Task<ContestSolution?> GetBestSolutionBySolverId(long problemId, string solverId)
     {
         Console.WriteLine($"GetBestSolutionBySolverId {problemId} {solverId}");
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -272,10 +193,24 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<ContestSolution?> GetBestSolutionByProblemId(long problemId)
+    public  async Task<List<ContestSolution>> GetAllSolutions()
+    {
+        Console.WriteLine("GetAllSolutions");
+        var response = await client.SessionExec(async session =>
+
+            await session.ExecuteDataQuery(
+                query: @"SELECT * FROM Solutions",
+                txControl: TxControl.BeginStaleRO().Commit()
+            ));
+        response.Status.EnsureSuccess();
+        Console.WriteLine("Done");
+        var queryResponse = (ExecuteDataQueryResponse) response;
+        return queryResponse.Result.ResultSets[0].Rows.Select(r => new ContestSolution(r)).ToList();
+    }
+
+    public  async Task<ContestSolution?> GetBestSolutionByProblemId(long problemId)
     {
         Console.WriteLine("GetBestSolutionByProblemId");
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -299,10 +234,9 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<string[]> GetAllSolvers(long problemId)
+    public  async Task<string[]> GetAllSolvers(long problemId)
     {
         Console.WriteLine("GetAllSolvers");
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -322,9 +256,21 @@ public static class SolutionRepo
         return queryResponse.Result.ResultSets[0].Rows.Select(x => (string?)x["solver_id"] ?? throw new Exception("WTF")).ToArray();
     }
 
-    public static async Task<long[]> GetAllProblems()
+    public  async Task<long[]> GetAllSolutionIds()
     {
-        var client = await CreateTableClient();
+        var response = await client.SessionExec(async session =>
+
+            await session.ExecuteDataQuery(
+                query: @"SELECT * FROM Solutions",
+                txControl: TxControl.BeginStaleRO().Commit(),
+                parameters: new Dictionary<string, YdbValue>()));
+        response.Status.EnsureSuccess();
+        var queryResponse = (ExecuteDataQueryResponse) response;
+        return queryResponse.Result.ResultSets[0].Rows.Select(x => (long?)x["problem_id"] ?? throw new Exception("WTF")).ToArray();
+    }
+
+    public  async Task<long[]> GetAllProblems()
+    {
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -335,14 +281,10 @@ public static class SolutionRepo
         var queryResponse = (ExecuteDataQueryResponse) response;
         return queryResponse.Result.ResultSets[0].Rows.Select(x => (long?)x["problem_id"] ?? throw new Exception("WTF")).ToArray();
     }
-
     public record ProblemStat(long problem_id, long? best_score, string? solver_id);
-    public record ProblemStatWithMeta(long problem_id, long? best_score, string? solver_meta);
 
-
-    public static async Task<List<ProblemStat>> GetAllBestStats()
+    public  async Task<List<ProblemStat>> GetAllBestStats()
     {
-        var client = await CreateTableClient();
         var response = await client.SessionExec(async session =>
 
             await session.ExecuteDataQuery(
@@ -366,42 +308,9 @@ public static class SolutionRepo
         return ans;
     }
 
-    public static async Task<List<ProblemStatWithMeta>> GetAllBestMetaStats()
+    private  async Task<TableClient> CreateTableClient()
     {
-        var client = await CreateTableClient();
-        var response = await client.SessionExec(async session =>
-
-            await session.ExecuteDataQuery(
-                query: @$"
-                SELECT
-  problem_id, solver_meta, score_estimated
-FROM (
-  SELECT
-    problem_id, solver_meta, score_estimated,
-    ROW_NUMBER() OVER(PARTITION BY problem_id ORDER BY score_estimated) AS row_number
-  FROM Solutions
-)
-WHERE row_number = 1;",
-                txControl: TxControl.BeginSerializableRW().Commit(),
-                parameters: new Dictionary<string, YdbValue>
-                {
-                }
-            ));
-        response.Status.EnsureSuccess();
-        var ans = new List<ProblemStatWithMeta>();
-        var queryResponse = (ExecuteDataQueryResponse) response;
-        foreach (var row in queryResponse.Result.ResultSets[0].Rows)
-        {
-            var problemId = (long?) row["problem_id"] ?? throw new ArgumentException();
-            var solverMeta = row["solver_meta"].GetOptionalJson();
-            var bestScore = (long?) row["score_estimated"] ?? throw new ArgumentException();
-            ans.Add(new ProblemStatWithMeta(problemId, bestScore, solverMeta));
-        }
-        return ans;
-    }
-
-    private static async Task<TableClient> CreateTableClient()
-    {
+        Console.WriteLine("CreateTableClient");
         var settings = new Settings();
         var config = new DriverConfig(
             endpoint: settings.YdbEndpoint,
@@ -418,5 +327,10 @@ WHERE row_number = 1;",
         await driver.Initialize();
 
         return new TableClient(driver, new TableClientConfig());
+    }
+
+    public SolutionRepo2()
+    {
+        client = CreateTableClient().GetAwaiter().GetResult();
     }
 }
